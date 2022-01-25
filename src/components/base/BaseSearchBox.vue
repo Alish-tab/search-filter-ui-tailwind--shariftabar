3d9d79257c1c0dd45644c69eac0a29b4b39ab56b
Alish-tab <alisharif.tab@gmail.com>
2022-01-25 19:59:13 +0330

    Responsive header
    full responsive header for mobile and desktop

--- src/components/base/BaseSearchBox.vue
+++ src/components/base/BaseSearchBox.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex items-center w-2/5 h-12 justify-self-start ml-auto mr-20">
+    <div class="flex items-center justify-self-start md:w-2/5 md:h-12 md:ml-auto md:mr-20">
 
         <!--------------------- search Input ----------------------------->
-        <input type="text" placeholder="جستجو در سوپرز...." class="w-full border rounded-3xl border-secondary shadow outline-none 
+        <input type="text" placeholder="جستجو در سوپرز...." class="hidden md:block w-full border rounded-3xl border-secondary shadow outline-none 
         placeholder:absolute placeholder:right-14 text-right p-3 pr-14 "/>
 
         <!--------------------- magnifier icon ---------------------------->
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,14 +1,14 @@
 <template>
- <div class="bg-white p-4 pl-12 pr-12 border-b border-secondary w-full h-20 flex items-center justify-around ">
+ <div class="bg-white p-4 pl-14 justify-between border-b border-secondary w-full h-14 flex items-center md:justify-around md:h-20 md:pl-12 md:pr-12">
      
      <!----------------------menue icon ------------------------->
-     <img src="../../assets/img/iconoir_menu.svg" alt="menu-icon" class="w-7 h-7 hover:cursor-pointer" >
+     <img src="../../assets/img/iconoir_menu.svg" alt="menu-icon" class=" w-6 h-6 hover:cursor-pointer order-2 md:w-7 md:h-7 md:order-none" >
 
      <!----------- SEARCH BOX ------------->
-     <search-box ></search-box>
+     <search-box class="order-0 md:order-none"></search-box>
 
-     <!------------------------superz logo -------------->
-    <a href="#"> <img src="../../assets/img/LogoType.svg" alt="Logo" class="w-32 h-8"> </a>
+     <!------------------------superz logo ----------------->
+    <a href="#"> <img src="../../assets/img/LogoType.svg" alt="Logo" class=" w-24 h-7 order-1 md:w-32 md:h-8 md:order-none"> </a>
 
 
  </div>
