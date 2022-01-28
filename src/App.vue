9c4698deb52abf79febb20f951c023380cf4a726
Alish-tab <alisharif.tab@gmail.com>
2022-01-28 18:01:55 +0330

    Add products component
    I added products component that's with this component we can control how the products are displayed

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
  <div>
      <my-header></my-header>
      <filters></filters>
+     <products></products>
  </div>
 </template>
 
 <script>
     import Header from './components/layout/Header.vue'
     import filters from './components/layout/Filters.vue'
+    import products from './components/layout/Products.vue'
     export default{
         components:{
             "my-header":Header,
             
-            filters
+            filters,
+            products
         }
     }
 </script>
---
+++ src/components/layout/Products.vue
@@ -0,0 +1,95 @@
+<template>
+    <section class="grid md:grid-cols-4 gap-y-11 md:w-9/12 ml-12 justify-between">
+        <product-card
+        v-for="product in products"
+        :key="product.title"
+        :title="product.title"
+        :price="product.price"
+        :discount="product.discount"
+        :discountedPrice="product.discountedPrice"
+        ></product-card>
+    </section>
+</template>
+
+<script>
+import ProductCard from '../base/BaseProductCard.vue'
+export default {
+    components:{
+        ProductCard
+    },
+    data() {
+        return{
+            products:[
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:true,
+                    discountedPrice:'11,480'
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:true,
+                    discountedPrice:'11,480'
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:false
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:false
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:true,
+                    discountedPrice:'11,480'
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:false
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:true,
+                    discountedPrice:'11,480'
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:true,
+                    discountedPrice:'11,480'
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:true,
+                    discountedPrice:'11,480'
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:false
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:true,
+                    discountedPrice:'11,480'
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                   discount:true,
+                    discountedPrice:'11,480'
+                },
+            ]
+        }
+    }
+}
+</script>
