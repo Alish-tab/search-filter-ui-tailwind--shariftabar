7e5a71a8f3638a267383397ddf526990802f7a57
Alish-tab <alisharif.tab@gmail.com>
2022-02-21 16:44:47 +0330

    migrate vuejs to Nuxtjs
    Bring codes from vue to nuxt

---
+++ components/products/Products.vue
@@ -0,0 +1,95 @@
+<template>
+    <section class="grid grid-cols-2 gap-x-5  md:grid-cols-3  lg:grid-cols-4  md:gap-x-3 lg:gap-x-20 gap-y-11 ">
+        <product-card
+        v-for="product in products"
+        :key="product.title"
+        :title="product.title"
+        :price="product.price"
+        :discount="product.discount"
+        :discountedPrice="product.discountedPrice"
+        ></product-card>
+    </section>
+</template>
+
+<script>
+import ProductCard from '../cards/BaseProductCard.vue'
+export default {
+    components:{
+        ProductCard
+    },
+    data() {
+        return{
+            products:[
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:true,
+                    discountedPrice:'11,480'
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:true,
+                    discountedPrice:'11,480'
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:false
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:false
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:true,
+                    discountedPrice:'11,480'
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:false
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:true,
+                    discountedPrice:'11,480'
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:true,
+                    discountedPrice:'11,480'
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:true,
+                    discountedPrice:'11,480'
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:false
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                    discount:true,
+                    discountedPrice:'11,480'
+                },
+                {
+                    title:'...اسم محصول',
+                    price:'16,000',
+                   discount:true,
+                    discountedPrice:'11,480'
+                },
+            ]
+        }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <the-header class="w-full"> </the-header>
+    <products class="w-11/12 ml-20 mb-16"> </products>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header/Header.vue'
+import Products from '@/components/products/Products.vue'
+export default {
+  components:{
+    'the-header': Header,
+    'products': Products
+ }
+}
+</script>
